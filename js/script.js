fc8d3d4dd9381f948d9a053395ca52f95ffe9fdc
unknown <stevekilosjb@gmail.com>
2023-01-16 11:20:45 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Software Developer","Database Management","Computer Network","Web Design"],
+    typeSpeed:120,
+    BackSpeed:60,
+    loop:true
+})
