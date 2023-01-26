5b7cb93dd9ece575d75e02baef3a66cc3bab7cd4
Stephen Kilonzo <stevekilosjb@gmail.com>
2023-01-26 09:58:39 +0300

    Added Data Science Service

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Software Developer","Database Management","Computer Network","Web Design"],
+    strings:[" ","Software Developer","Database Administrator","Computer Network Specialist","Web Designer", "Data Scientist"],
     typeSpeed:120,
     BackSpeed:60,
     loop:true
